531bd8d5a83695c967af6239404ae19dd57e1ee3
ubu-1-git <kobi.c@hotmail.com>
2024-12-11 20:11:50 +0800

    fix services scaling

--- src/static/Service.tsx
+++ src/static/Service.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 function Service(serviceData: ServiceData) {
   return (
     <div className="flex min-h-full flex-col desktop:w-1/3 m-4 mobile:w-3/4 p-8 rounded-md transition-colors hover:bg-slate-700">
-      <div className="relative w-2/3 h-auto self-center p-14">
+      <div className="relative w-2/3 h-auto self-center p-6">
         <Image
           src={serviceData.img}
           alt={serviceData.alt}
